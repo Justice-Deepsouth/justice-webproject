8b75056629c819110b898de9aed69a07d5935b35
Ruchdee <ruchdee.b@psu.ac.th>
2018-12-20 14:18:17 +0700

    update logo

--- index.php
+++ index.php
@@ -66,7 +66,7 @@
 			<div class="top-menu">
 				<div class="row">
 					<div class="col-xs-2">
-						<div id="fh5co-logo"><a href="index.php"><img src="images/logo_7.jpg"></a></div>
+						<div id="fh5co-logo"><a href="index.php"><img src="images/6646.jpg"></a></div>
 					</div>
 					<div class="col-xs-10 text-right menu-1">
 						<ul>
